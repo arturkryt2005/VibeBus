b16019084a2448371a69425ac52f322afd1aa21b
Artur <arturbakasov1@gmail.com>
2023-12-02 20:21:01 +0300

    Add context

---
+++ VipeBus.Application/Entities/Users/User.cs
@@ -0,0 +1,15 @@
+﻿namespace VipeBus.Application.Entities.Users
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ VipeBus.Core/VipeBusContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity;
+using VipeBus.Application.Entities.Users;
+
+namespace VipeBus.Core
+{
+    public class VipeBusContext : DbContext
+    {
+        public VipeBusContext() : base("DbConnection")
+        {
+
+        }
+
+        DbSet<User> Users { get; set; }
+    }
+}
