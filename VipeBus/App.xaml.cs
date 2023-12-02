71f307df8e3236055e3b9b22b50a0bfe4fb6d281
Artur <arturbakasov1@gmail.com>
2023-12-02 21:42:34 +0300

    Fix login

--- VipeBus/App.xaml.cs
+++ VipeBus/App.xaml.cs
@@ -5,7 +5,7 @@ namespace VipeBus
     /// <summary>
     /// Логика взаимодействия для App.xaml
     /// </summary>
-    public partial class App : Application
+    public partial class App : System.Windows.Application
     {
     }
 }
